b8de168fea9cb7aed6a894caeb45065882f199d2
GitHub Action <action@github.com>
2021-02-25 16:59:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/saloed/customdiffplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/saloed/customdiffplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.saloed.customdiffplugin.services
 
+import com.github.saloed.customdiffplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
